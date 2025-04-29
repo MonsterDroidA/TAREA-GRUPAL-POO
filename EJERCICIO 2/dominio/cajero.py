1acaeeb7f3b52e26a4028d40a2fb93df532c3614
Alain Machuca <alainisai3@gmail.com>
2025-04-28 21:48:42 -0500

    correccion de archivos innecesarios

--- EJERCICIO 2/dominio/cajero.py
+++
@@ -1,6 +0,0 @@
-class Cajero:
-    def __init__(self, nombre):
-        self.nombre = nombre
-
-    def atender_cliente(self, cliente):
-        print(f"La persona {cliente.nombre} ha sido atendida por {self.nombre}.")
